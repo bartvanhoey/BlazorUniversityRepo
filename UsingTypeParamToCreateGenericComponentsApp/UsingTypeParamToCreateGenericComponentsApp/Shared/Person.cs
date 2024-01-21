381ea765d31a1c49fd6375651327a9b293cf1398
ekapituro <ekapituro@gmail.com>
2024-01-21 07:49:48 +0100

    Update

---
+++ UsingTypeParamToCreateGenericComponentsApp/UsingTypeParamToCreateGenericComponentsApp/Shared/Person.cs
@@ -0,0 +1,10 @@
+namespace UsingTypeParamToCreateGenericComponentsApp.Shared
+{
+    public class Person
+    {
+        public string? Salutation { get; set; }
+
+        public string? GivenName { get; set; }
+        public string? FamilyName { get; set; }
+    }
+}
