468fd697afb146312bdf90bd1e0c306a6f116181
Bart Van Hoey <bart.vanhoey@graw.de>
2024-01-23 10:51:51 +0100

    Update

---
+++ PassPlaceholdersToRenderFragmentsApp/PassPlaceholdersToRenderFragmentsApp/Person.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassPlaceholdersToRenderFragmentsApp
+{
+    public class Person
+    {
+        public string? Salutation { get; set; }
+        public string? GivenName { get; set; }
+        public string? FamilyName { get; set; }
+    }
+
+}
